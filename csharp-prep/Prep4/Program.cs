95f78409160f48f6bb43ac6bf9a425c8fb124b1a
leviticus2025 <bea24018@byui.edu>
2025-09-25 17:56:59 -0600

    prep 4 for lists. I added the code for summing the total, the average (as a float) and also the maximum number

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+        Console.WriteLine("Enter a list of numbers: type 0 when done!");
+        int input = 1;
+        List<int> numbers = new List<int>();
+        int sum = 0;
+        float average = 0;
+        int max = 0;
+        while (input != 0)
+        {
+            Console.WriteLine("Enter a number: ");
+            input = int.Parse(Console.ReadLine());
+            if (input != 0)
+            {
+                numbers.Add(input); //to not add the ending 0
+            }
+        }
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The sum is {sum}");
+        average = (float)sum / numbers.Count;
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The maximum is {max}");
+        }
 }

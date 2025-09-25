fa6b54d271c7f6d1de9b24357113e82f91b7947a
leviticus2025 <bea24018@byui.edu>
2025-09-25 17:01:46 -0600

    prep 3 for magic number in program.cs with added stretch challenges

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Welcome to the Number Guessing Game!");
+
+        string play_again = "yes";
+        while (play_again == "yes")
+        {
+            Random randomGenerator = new Random(); // random number
+            int magic_number = randomGenerator.Next(1, 100); 
+            int guess = -11;
+            int count = 0;
+            while (guess != magic_number)
+            {
+                Console.WriteLine("What is your guess? ");
+                guess = Convert.ToInt32(Console.ReadLine());
+                count++;
+                if (guess < magic_number)
+                {
+                    Console.WriteLine("higher");
+                }
+                else if (guess > magic_number)
+                {
+                    Console.WriteLine("lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+
+            }
+            Console.WriteLine($"You guessed {count} times");
+            Console.WriteLine("Play again? (yes/no)");
+            play_again = Console.ReadLine();
+        }
     }
 }

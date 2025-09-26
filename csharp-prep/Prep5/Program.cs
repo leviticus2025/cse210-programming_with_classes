3cda82f1e736c83f246b769f094941ad8509f242
leviticus2025 <bea24018@byui.edu>
2025-09-25 18:19:34 -0600

    prep 5 for functions, ones that return and also ones that don't. created all functions then called them in my main function

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+        int birth_year;
+        PromptUserBirthYear(out birth_year);
+        DisplayResult(userName, squaredNumber, birth_year);
+        
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("What is your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+        static int PromptUserNumber()
+        {
+            Console.Write("What is your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+        static void PromptUserBirthYear(out int birth_year)
+        {
+            Console.Write("Enter your birth year: ");
+            birth_year = int.Parse(Console.ReadLine());
+        }
+        static int SquareNumber(int number)
+        {
+            int number_squared = number * number;
+            return number_squared;
+        }
+        static void DisplayResult(string name, int number_squared, int birth_year)
+        {
+            Console.WriteLine($"{name}, the square of your number is {number_squared}.");
+            Console.WriteLine($"{name}, you will be {2025 - birth_year} years old this year.");
+        }
     }
 }
